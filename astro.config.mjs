abddffc07702d0faf4511ad1136fd5181846a2dc
greciart <greciavpv@gmail.com>
2025-07-20 15:39:31 -0400

    Fix package.json syntax and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,33 @@
+// @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import astroIcon from 'astro-icon';
+import mdx from '@astrojs/mdx';
+import playformCompress from "@playform/compress";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "static",
+  integrations: [
+    tailwind(),
+    mdx(),
+    astroIcon({
+      include: {
+        mdi: ["*"],
+        ri: ['*'],
+        'simple-icons': ['*'],
+      },
+    }),
+    playformCompress({
+      CSS: false,
+      Image: false,
+      Action: {
+        Passed: async () => true,   // https://github.com/PlayForm/Compress/issues/376
+      },
+    })
+  ],
+  output: "hybrid",
+  adapter: vercel(),
   site: "https://greciart.github.io",
   base: '/',
 });
